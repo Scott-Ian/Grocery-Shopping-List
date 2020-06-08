69079c061dc0076e07c2071b293fdcc0134e7087
Ian Scott <chesnekov@gmail.com>
2020-06-08 14:49:53 -0700

    make form take in any list of groceries, splits the string into an array, and then displays in a list, while hidding the form
    Co-authored-by: Sara Kane <kane.sarak@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,13 @@ $("document").ready(function() {
     const unSlicedList = $("textarea#food").val();
 
     console.log(unSlicedList);
+    
+    const slicedFood = unSlicedList.split(","); 
+    slicedFood.forEach(function (food) {
+      $(".shopping-list").append(`<li> ${food} </li>`);
+    });
 
     $("form#groceries").hide();
+    $(".shopping").show();
   });
 });
