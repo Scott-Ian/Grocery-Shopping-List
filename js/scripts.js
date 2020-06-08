fd9c334db152e9521713a78528970a7bcfbc5ef7
Ian Scott <chesnekov@gmail.com>
2020-06-08 14:36:08 -0700

    creat text area, collect and store groceries into one string, and hide textarea upon form submission
    Co-authored-by: Sara Kane <kane.sarak@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -2,5 +2,10 @@ $("document").ready(function() {
   $("form#groceries").submit(function(event) {
     event.preventDefault();
 
+    const unSlicedList = $("textarea#food").val();
+
+    console.log(unSlicedList);
+
+    $("form#groceries").hide();
   });
 });
